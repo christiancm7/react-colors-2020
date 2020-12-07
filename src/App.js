284bc840b84dd60a4041c548e09e7441371e7799
Christian <atticuschris@yahoo.com>
2020-12-06 21:17:11 -0600

    created new palette form route and component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom'
 import Pallete from './Palette'
 import PaletteList from './PaletteList'
 import SingleColorPalette from './SingleColorPalette'
+import NewPaletteForm from './NewPalletForm'
 import { seedColors } from './seedColors'
 import { generatePalette } from './colorHelpers'
 
@@ -16,6 +17,11 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route 
+          exact 
+          path="/palette/new"
+          render={() => <NewPaletteForm />} 
+        />
         <Route 
           exact 
           path="/palette/:paletteId/:colorId" 
---
+++ src/NewPalletForm.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class NewPalletForm extends Component {
+    render() {
+        return (
+            <div>
+                New Palette Form
+            </div>
+        )
+    }
+}
+export default NewPalletForm;
