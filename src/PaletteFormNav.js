7eec1ab92d4a5757f969910490006f3d304c0edb
Christian <atticuschris@yahoo.com>
2020-12-08 08:53:39 -0600

    fixed cancel button and brought in emoji-mart picker

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -1,87 +1,124 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom';
-import classNames from "classnames";
+import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
-import Drawer from "@material-ui/core/Drawer";
+import PaletteMetaForm from "./PaletteMetaForm";
+import { Link } from "react-router-dom";
+import classNames from "classnames";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import Button from "@material-ui/core/Button";
-import DraggableColorList from './DraggableColorList' 
-import { ValidatorForm, TextValidator } from "react-material-ui-form-validator";
-import { ChromePicker } from "react-color";
-import { arrayMove } from 'react-sortable-hoc';
+
+const drawerWidth = 400;
+const styles = theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "64px"
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginLeft: 12,
+    marginRight: 20
+  },
+  navBtns: {
+    marginRight: "1rem",
+    "& a": {
+      textDecoration: "none"
+    }
+  },
+  button: {
+    margin: "0 0.5rem"
+  }
+});
 
 class PaletteFormNav extends Component {
-  state = {
-    newPaletteName: ""
+  constructor(props) {
+    super(props);
+    this.state = { newPaletteName: "", formShowing: false };
+    this.handleChange = this.handleChange.bind(this);
+    this.showForm = this.showForm.bind(this);
   }
 
-  componentDidMount(){
-    ValidatorForm.addValidationRule("isPaletteNameUnique", value =>
-      this.props.palettes.every(
-        ({ paletteName }) => paletteName.toLowerCase() !== value.toLowerCase()
-      )
-    );
-  }
-
-  handleChange = (evt) => {
+  handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
     });
   }
+  showForm() {
+    this.setState({ formShowing: true });
+  }
 
-    render() {
-      const { classes, open, handleSubmit, handleDrawerOpen } = this.props;
-      const { newPaletteName } = this.state;
-        return (
-            <div>
-                <CssBaseline />
-                <AppBar
-                  position='fixed'
-                  color='default'
-                  className={classNames(classes.appBar, {
-                    [classes.appBarShift]: open
-                  })}
-                >
-                  <Toolbar disableGutters={!open}>
-                    <IconButton
-                      color='inherit'
-                      aria-label='Open drawer'
-                      onClick={handleDrawerOpen}
-                      className={classNames(classes.menuButton, open && classes.hide)}
-                    >
-                      <MenuIcon />
-                    </IconButton>
-                    <Typography variant='h6' color='inherit' noWrap>
-                      Persistent drawer
-                    </Typography>
-                    <ValidatorForm onSubmit={() => handleSubmit(newPaletteName)}>
-                      <TextValidator
-                        label='Palette Name'
-                        value={newPaletteName}
-                        name='newPaletteName'
-                        onChange={this.handleChange}
-                        validators={["required", "isPaletteNameUnique"]}
-                        errorMessages={["Enter Palette Name", "Name already used"]}
-                      />
-                      <Button variant='contained' color='primary' type='submit'>
-                        Save Palette
-                      </Button>
-                      <Link to="/">
-                        <Button variant='contained' color='secondary'>GO BACK</Button>
-                      </Link>
-                    </ValidatorForm>
-                  </Toolbar>
-                </AppBar>
-            </div>
-        )
-    }
+  hideForm =() => {
+    this.setState({ formShowing: false });
+  }
+  render() {
+    const { classes, open, palettes, handleSubmit } = this.props;
+    return (
+      <div className={classes.root}>
+        <CssBaseline />
+        <AppBar
+          position='fixed'
+          color='default'
+          className={classNames(classes.appBar, {
+            [classes.appBarShift]: open
+          })}
+        >
+          <Toolbar disableGutters={!open}>
+            <IconButton
+              color='inherit'
+              aria-label='Open drawer'
+              onClick={this.props.handleDrawerOpen}
+              className={classNames(classes.menuButton, open && classes.hide)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant='h6' color='inherit' noWrap>
+              Create A Palette
+            </Typography>
+          </Toolbar>
+          <div className={classes.navBtns}>
+            <Link to='/'>
+              <Button
+                variant='contained'
+                color='secondary'
+                className={classes.button}
+              >
+                Go Back
+              </Button>
+            </Link>
+            <Button
+              variant='contained'
+              color='primary'
+              onClick={this.showForm}
+              className={classes.button}
+            >
+              Save
+            </Button>
+          </div>
+        </AppBar>
+        {this.state.formShowing && (
+          <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} hideForm={this.hideForm}/>
+        )}
+      </div>
+    );
+  }
 }
-
-export default PaletteFormNav;
+export default withStyles(styles, { withTheme: true })(PaletteFormNav);
