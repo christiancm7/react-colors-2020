c873e69047ad9918189e03f437136e849991f45c
Christian <atticuschris@yahoo.com>
2020-12-09 08:57:04 -0600

    deleteIcon setup on minipalette

--- src/DraggableColorList.js
+++ src/DraggableColorList.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react'
+import React from 'react'
 import { SortableContainer } from 'react-sortable-hoc'
 import DraggableColorBox from './DraggableColorBox'
 
--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/styles'
 import { styles } from './styles/MiniPaletteStyles'
+import DeleteIcon from '@material-ui/icons/Delete'
 
 const MiniPalette = (props) => {
     const { classes, paletteName, emoji, colors, handleClick } = props;
@@ -16,6 +17,9 @@ const MiniPalette = (props) => {
     })
     return (
         <div className={classes.root} onClick={handleClick}>
+            <div className={classes.delete}>
+                <DeleteIcon className={classes.deleteIcon} style={{transition: "all 0.3s ease-in-out"}} />
+            </div>
             <div className={classes.colors}>{miniColorBoxes}</div>
             <h5 className={classes.title}>
                 {paletteName}<span className={classes.emoji}>{emoji}</span>
